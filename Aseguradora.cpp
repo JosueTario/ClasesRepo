bc77dec751613d6eeabeec58747187299be8ebd4
JosueTario <00092625@uca.edu.sv>
2025-05-08 13:19:07 -0600

    Add files via upload

--- Aseguradora.cpp
+++ Aseguradora.cpp
@@ -52,31 +52,37 @@ int main(){
         MontoSeguro = 10,000.00;
         cout<<"Su monto de seguro es de $10,000 "<<endl;
         cout<<"En caso de muerte por cualquier razon, se le pagara la cantidad de dinero mostrada anteriormente";
+
       }else if (sueldo>=910.01 && sueldo<=1255.00)
       {
         MontoSeguro = 14,000.00;
         cout<<"Su monto de seguro es de $14,000.00 ";
         cout<<"En caso de muerte por cualquier razon, se le pagara la cantidad de dinero mostrada anteriormente";
+
       }else if (sueldo>=1255.01 && sueldo<=1630.00)
       {
         MontoSeguro = 18,000.00;
         cout<<"Su monto de seguro es de $18,000.00 ";
         cout<<"En caso de muerte por cualquier razon, se le pagara la cantidad de dinero mostrada anteriormente";
+
       }else if (sueldo>=1630.00)
       {
         MontoSeguro = 22,000.00;
         cout<<"Su monto de seguro es de $22,000.00 ";
-        cout<<"En caso de muerte por cualquier razon, se le pagara la cantidad de dinero mostrada anteriormente";
-      }else{
-        cout<<"Sueldo invalido, ingrese un sueldo de $365 o mas";
+        cout<<"En caso de muerte por cualquier razon, se le pagara la cantidad de dinero mostrada anteriormente "<<endl;
+
       }
+      
 
             cout<<"El valor asegurado se le entregara dependiendo de la cantidad dinero que le corresponda. Usted recibira el %50 en un solo pago, y el otro %50 se le dara en 12 cuotas mensuales "<<endl;
             
             anticipacion=MontoSeguro/2;
+            
             cout<<"Su anticipacion sera de: "<<anticipacion<<endl;
             RestoDelPago=anticipacion/12;
+
             cout<<"Quiere decir que despues de la anticipacion se le pagara una cantidad durante 12 meses, dicha cantidad sera de: "<<RestoDelPago<<endl;
+
       break;
 
       case 'c':
